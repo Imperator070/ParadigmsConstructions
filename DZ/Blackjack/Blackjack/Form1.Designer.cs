1abf84b4558a506d017bd86f4e59e6c42a6a9ae9
Imperator070 <k.bykov.06@mail.ru>
2025-12-18 13:03:18 +0300

    DZ made

---
+++ DZ/Blackjack/Blackjack/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Blackjack
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button hitButton;
+        private System.Windows.Forms.Button standButton;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Label playerCardsLabel;
+        private System.Windows.Forms.Label dealerCardsLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label playerScoreLabel;
+        private System.Windows.Forms.Label dealerScoreLabel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.hitButton = new System.Windows.Forms.Button();
+            this.standButton = new System.Windows.Forms.Button();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.playerCardsLabel = new System.Windows.Forms.Label();
+            this.dealerCardsLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.playerScoreLabel = new System.Windows.Forms.Label();
+            this.dealerScoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // hitButton
+            // 
+            this.hitButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.hitButton.Location = new System.Drawing.Point(30, 280);
+            this.hitButton.Name = "hitButton";
+            this.hitButton.Size = new System.Drawing.Size(100, 40);
+            this.hitButton.TabIndex = 0;
+            this.hitButton.Text = "Hit";
+            this.hitButton.UseVisualStyleBackColor = true;
+            this.hitButton.Click += new System.EventHandler(this.hitButton_Click);
+            // 
+            // standButton
+            // 
+            this.standButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.standButton.Location = new System.Drawing.Point(140, 280);
+            this.standButton.Name = "standButton";
+            this.standButton.Size = new System.Drawing.Size(100, 40);
+            this.standButton.TabIndex = 1;
+            this.standButton.Text = "Stand";
+            this.standButton.UseVisualStyleBackColor = true;
+            this.standButton.Click += new System.EventHandler(this.standButton_Click);
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.newGameButton.Location = new System.Drawing.Point(250, 280);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(100, 40);
+            this.newGameButton.TabIndex = 2;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // playerCardsLabel
+            // 
+            this.playerCardsLabel.AutoSize = true;
+            this.playerCardsLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold);
+            this.playerCardsLabel.Location = new System.Drawing.Point(30, 40);
+            this.playerCardsLabel.Name = "playerCardsLabel";
+            this.playerCardsLabel.Size = new System.Drawing.Size(80, 16);
+            this.playerCardsLabel.TabIndex = 3;
+            this.playerCardsLabel.Text = "Player: ";
+            // 
+            // dealerCardsLabel
+            // 
+            this.dealerCardsLabel.AutoSize = true;
+            this.dealerCardsLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold);
+            this.dealerCardsLabel.Location = new System.Drawing.Point(30, 120);
+            this.dealerCardsLabel.Name = "dealerCardsLabel";
+            this.dealerCardsLabel.Size = new System.Drawing.Size(80, 16);
+            this.dealerCardsLabel.TabIndex = 4;
+            this.dealerCardsLabel.Text = "Dealer: ";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.resultLabel.ForeColor = System.Drawing.Color.Navy;
+            this.resultLabel.Location = new System.Drawing.Point(30, 180);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(320, 30);
+            this.resultLabel.TabIndex = 5;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // playerScoreLabel
+            // 
+            this.playerScoreLabel.AutoSize = true;
+            this.playerScoreLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic);
+            this.playerScoreLabel.Location = new System.Drawing.Point(30, 60);
+            this.playerScoreLabel.Name = "playerScoreLabel";
+            this.playerScoreLabel.Size = new System.Drawing.Size(46, 15);
+            this.playerScoreLabel.TabIndex = 6;
+            this.playerScoreLabel.Text = "Score: 0";
+            // 
+            // dealerScoreLabel
+            // 
+            this.dealerScoreLabel.AutoSize = true;
+            this.dealerScoreLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic);
+            this.dealerScoreLabel.Location = new System.Drawing.Point(30, 140);
+            this.dealerScoreLabel.Name = "dealerScoreLabel";
+            this.dealerScoreLabel.Size = new System.Drawing.Size(86, 15);
+            this.dealerScoreLabel.TabIndex = 7;
+            this.dealerScoreLabel.Text = "Dealer Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 340);
+            this.Controls.Add(this.dealerScoreLabel);
+            this.Controls.Add(this.playerScoreLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.dealerCardsLabel);
+            this.Controls.Add(this.playerCardsLabel);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.standButton);
+            this.Controls.Add(this.hitButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "♠ Blackjack ♣";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ DZ/Blackjack/Blackjack/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Blackjack
+{
+    public partial class Form1 : Form
+    {
+        private Deck deck;
+        private Hand playerHand;
+        private Hand dealerHand;
+        private bool gameOver = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            deck = new Deck();
+            deck.Shuffle();
+            playerHand = new Hand();
+            dealerHand = new Hand();
+            gameOver = false;
+
+            playerHand.AddCard(deck.Deal());
+            playerHand.AddCard(deck.Deal());
+            dealerHand.AddCard(deck.Deal());
+            dealerHand.AddCard(deck.Deal());
+
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            playerCardsLabel.Text = "Player: " + string.Join(", ", playerHand.Cards);
+            playerScoreLabel.Text = $"Score: {playerHand.GetScore()}";
+
+            if (gameOver)
+            {
+                dealerCardsLabel.Text = "Dealer: " + string.Join(", ", dealerHand.Cards);
+                dealerScoreLabel.Text = $"Dealer Score: {dealerHand.GetScore()}";
+            }
+            else
+            {
+                dealerCardsLabel.Text = "Dealer: " + dealerHand.Cards[0] + ", [Hidden]";
+                dealerScoreLabel.Text = $"Dealer Score: {dealerHand.Cards[0].GetValue()}";
+            }
+            resultLabel.Text = "";
+        }
+
+        private void hitButton_Click(object sender, EventArgs e)
+        {
+            playerHand.AddCard(deck.Deal());
+            UpdateUI();
+
+            if (playerHand.GetScore() > 21)
+            {
+                resultLabel.Text = "Player busts! Dealer wins.";
+                gameOver = true;
+                hitButton.Enabled = false;
+                standButton.Enabled = false;
+            }
+        }
+
+        private void standButton_Click(object sender, EventArgs e)
+        {
+            gameOver = true;
+            while (dealerHand.GetScore() < 17)
+            {
+                dealerHand.AddCard(deck.Deal());
+            }
+            UpdateUI();
+            CheckResult();
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            NewGame();
+            hitButton.Enabled = true;
+            standButton.Enabled = true;
+        }
+
+        private void CheckResult()
+        {
+            int playerScore = playerHand.GetScore();
+            int dealerScore = dealerHand.GetScore();
+
+            if (playerScore > 21)
+            {
+                resultLabel.Text = "Player busts! Dealer wins.";
+            }
+            else if (dealerScore > 21)
+            {
+                resultLabel.Text = "Dealer busts! Player wins!";
+            }
+            else if (playerScore > dealerScore)
+            {
+                resultLabel.Text = "Player wins!";
+            }
+            else if (dealerScore > playerScore)
+            {
+                resultLabel.Text = "Dealer wins.";
+            }
+            else
+            {
+                resultLabel.Text = "Push (tie).";
+            }
+        }
+    }
+
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Rank { get; set; }
+
+        public Card(string suit, string rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+        public int GetValue()
+        {
+            switch (Rank)
+            {
+                case "J":
+                case "Q":
+                case "K":
+                    return 10;
+                case "A":
+                    return 11;
+                default:
+                    return int.Parse(Rank);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Rank} of {Suit}";
+        }
+    }
+
+    public class Deck
+    {
+        private List<Card> cards;
+        private Random rng = new Random();
+
+        public Deck()
+        {
+            cards = new List<Card>();
+            string[] suits = { "♠", "♥", "♦", "♣" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+            foreach (var suit in suits)
+            {
+                foreach (var rank in ranks)
+                {
+                    cards.Add(new Card(suit, rank));
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            int n = cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                (cards[k], cards[n]) = (cards[n], cards[k]);
+            }
+        }
+
+        public Card Deal()
+        {
+            if (cards.Count == 0)
+                throw new InvalidOperationException("Deck is empty");
+            Card card = cards[0];
+            cards.RemoveAt(0);
+            return card;
+        }
+    }
+
+    public class Hand
+    {
+        public List<Card> Cards { get; private set; }
+
+        public Hand()
+        {
+            Cards = new List<Card>();
+        }
+
+        public void AddCard(Card card)
+        {
+            Cards.Add(card);
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+            int aces = 0;
+
+            foreach (var card in Cards)
+            {
+                if (card.Rank == "A")
+                {
+                    aces++;
+                    score += 11;
+                }
+                else
+                {
+                    score += card.GetValue();
+                }
+            }
+
+            while (score > 21 && aces > 0)
+            {
+                score -= 10;
+                aces--;
+            }
+
+            return score;
+        }
+    }
+}
